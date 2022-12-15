2aeaedd621d88a5c7721dcf97f1c5e19f91406e5
chiaramery <meringolachiara@gmail.com>
2022-12-15 16:24:08 +0100

    sezione card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="jumbotron"></section>
+    <section class="jumbotron">
+        <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="">
+    </section>
+
+    <section class="series">
+        <div class="container">
+            <div class="row row-cols-6 pt-3">
+                @foreach ($data as $item)
+                    <div class="col mb-3">
+                        <div class="series-card">
+                            <img src="{{ $item['thumb'] }}" alt="{{ $item['series'] }}">
+                            <h4 class="text-center">{{ strtoupper($item['series']) }}</h4>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
