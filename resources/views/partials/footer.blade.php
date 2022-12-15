d999fe6795af0a4c68391f96579d8419c2069447
chiaramery <meringolachiara@gmail.com>
2022-12-15 16:56:33 +0100

    Home

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,68 @@
 <footer>
-    footer
+    <section class="footer-top">
+        <div class="container-footer">
+            <div class="row row-cols-3 pt-5">
+                <div class="col">
+                    <h5>DC COMICS</h5>
+                    <p>Characters</p>
+                    <p>Comics</p>
+                    <p>Movies</p>
+                    <p>TV</p>
+                    <p>Games</p>
+                    <p>Videos</p>
+                    <p>News</p>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h5>DC</h5>
+                    <p>Terms Of Use</p>
+                    <p>Privacy Popcy(New)</p>
+                    <p>Ad Choices</p>
+                    <p>Advertising</p>
+                    <p>Jobs</p>
+                    <p>Subscriptions</p>
+                    <p>Talent Workshops</p>
+                    <p>CPSC Certificates</p>
+                    <p>Rantings</p>
+                    <p>Shop Help</p>
+                    <p>Contact Us</p>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h5>SITES</h5>
+                    <p>DC</p>
+                    <p>MAD</p>
+                    <p>DC Kids</p>
+                    <p>DC Universe</p>
+                    <p>DC Power Visa</p>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h5>SHOP</h5>
+                    <p>Shop DC</p>
+                    <p>Shop DC Collectibles</p>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="footer-bottom">
+        <div class="container d-flex justify-content-around">
+            <div class="left">
+                <button>SIGN-UP NOW!</button>
+            </div>
+            <div class="right">
+                <h1 class="d-flex align-items-center">FOLLOW US</h1>
+                <div class="icon">
+                    <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="">
+                    <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                    <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                    <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="">
+                    <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
 </footer>
