b9a9edd038d56544ee099e7aa9f4e5f81a320d62
chiaramery <meringolachiara@gmail.com>
2022-12-15 17:15:47 +0100

    Implemanetata pagina Characters

--- resources/views/characters.blade.php
+++ resources/views/characters.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <section>
+    <section class="movie">
         <div class="container">
-            <h2>Pagina Characters</h2>
+            <div class="row row-cols-3 pt-3">
+                @foreach ($data as $item)
+                    <div class="col mb-3">
+                        <div class="series-card">
+                            <h4 class="text-center">{{ strtoupper($item['series']) }}</h4>
+                            <p class="text-center">Sale date: {{ $item['sale_date'] }}</p>
+                            <p class="text-center">Type: {{ $item['type'] }}</p>
+
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
 
     </section>
