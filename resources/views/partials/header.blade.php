4a79f863af39ae973d88f2655013b3b3075668e1
chiaramery <meringolachiara@gmail.com>
2022-12-15 15:50:42 +0100

    Header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,25 +1,25 @@
 <header>
     <div class="container">
-        <!-- LOGO -->
-        <div class="logo">
-            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
-            <!-- NAV -->
-        </div>
-        <nav>
-            <ul class="d-flex justify-content-center">
+        <!-- NAV -->
+        <nav class="d-flex justify-content-around">
+            <!-- LOGO -->
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+            <ul class="d-flex align-items-center">
                 <li class="mx-4">
                     <a href="{{ route('home') }}">HOME</a>
                 </li>
                 <li class="mx-4">
-                    <a href="{{ route('characters') }}">Characters</a>
+                    <a href="{{ route('characters') }}">CHARACTERS</a>
                 </li>
                 <li class="mx-4">
-                    <a href="{{ route('movies') }}">Movies</a>
+                    <a href="{{ route('movies') }}">MOVIES</a>
                 </li>
             </ul>
 
+            <input type="text" placeholder="search" class="align-self-center">
         </nav>
-        <input type="text" placeholder="search">
 
     </div>
 </header>
